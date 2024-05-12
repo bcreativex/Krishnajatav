322c4e4c1b9c7378c6a4f0eaf01c7893332e88fd
krishna <krishnajatav504@gmail.com>
2024-05-12 13:02:31 +0530

    more corrections in orientations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-/* toggle icon navbar */
+/*================= toggle icon navbar ==============*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
 
+
+// scroll section 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -22,6 +31,10 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*================= remove toggle icon  & navbar  jab navbar pe click kiya ya scroll==============*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
 
