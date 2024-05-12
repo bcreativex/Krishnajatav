387ac77d0b3090b3d0ddd6e46218a8262487e4a2
krishna <krishnajatav504@gmail.com>
2024-05-12 14:09:10 +0530

    animations and scroll added

--- js/script.js
+++ js/script.js
@@ -37,5 +37,20 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
+/*================= scroll reveal ==============*/
+
+ScrollReveal({
+    // reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 200
+     });
+
+     ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+     ScrollReveal().reveal('.home-img, .servies-container, .portfolio-box, .contact form', { origin: 'bottom' });
+     ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+     ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
 
 
